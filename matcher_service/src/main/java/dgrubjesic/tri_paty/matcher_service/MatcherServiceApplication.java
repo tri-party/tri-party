b84c55a1a596e2f3cf77ff7758c181c0dc65768d
dusangrubjesic <dusancoder@gmail.com>
2023-01-18 19:24:59 +0100

    matcher service

---
+++ matcher_service/src/main/java/dgrubjesic/tri_paty/matcher_service/MatcherServiceApplication.java
@@ -0,0 +1,13 @@
+package dgrubjesic.tri_paty.matcher_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatcherServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatcherServiceApplication.class, args);
+    }
+
+}
---
+++ matcher_service/src/test/java/dgrubjesic/tri_paty/matcher_service/MatcherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dgrubjesic.tri_paty.matcher_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatcherServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
